66637a8fd15ccc1bb38ea6fc1b24c8648fa5a1b4
Pascal Pater <pascal.pater@gmail.com>
2021-07-06 00:35:03 +0200

    setup basic Route in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Dashboard from "./components/Dashboard";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="app">
-      <h1>App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <Dashboard />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
