119c7eb7205de5788e3db0435d7cfdc82cd18696
Pascal Pater <pascal.pater@gmail.com>
2021-07-06 00:26:29 +0200

    add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        &copy; 2021 - Pascal Pater - Eindopdracht React Student Dashboard voor
+        Winc Academy Front-end Development opleiding
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
