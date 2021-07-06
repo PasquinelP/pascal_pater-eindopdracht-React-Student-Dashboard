41253794806c4446357c45049077f6842a913ee4
Pascal Pater <pascal.pater@gmail.com>
2021-07-06 16:30:17 +0200

    add states assignmentTypes

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -5,11 +5,23 @@ export const AppContext = createContext();
 
 export const AppProvider = (props) => {
   
+  // put students data set in state
   const [students, setStudents] = useState(studentsData);
+
+  // extract list of assignment types from data set
+  const assignments = students.map((student) => student.assignment);
+  const uniqueAssignments = [...new Set(assignments)];
+  const assignmentTypeList = uniqueAssignments.map((assignment, index) => ({
+    id: index + 1,
+    assignmentType: assignment,
+  }));
+
+  // put assignment types in state
+  const [assignmentTypes, setAssignmentTypes] = useState(assignmentTypeList);
 
 
   return (
-    <AppContext.Provider value={{ students, setStudents }}>
+    <AppContext.Provider value={{ students, setStudents, assignmentTypes, setAssignmentTypes }}>
       {props.children}
     </AppContext.Provider>
   );
