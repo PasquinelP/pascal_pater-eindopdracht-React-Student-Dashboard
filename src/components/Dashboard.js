25cead41bc3e5c7bfc3dfbedd8c8e8a0282944c9
Pascal Pater <pascal.pater@gmail.com>
2021-07-06 14:54:24 +0200

    quick test to see if context works and we get data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "./AppContext";
 
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+
+  const { students } = useContext(AppContext);
+
+  const names = students.map((student) => student.name);
+  const uniqueNames = [...new Set(names)];
+  const studentsNames = uniqueNames.map((student, index) => ( {
+    id: index,
+    name: student,
+  }
+  ));
+
+ console.log("Students data is: ", students);
+ console.log(studentsNames)
+
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+    </>
+  );
 };
 
 export default Dashboard;
