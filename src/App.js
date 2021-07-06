5e718ab706b111cc105de6483a2e45e49f57746d
Pascal Pater <pascal.pater@gmail.com>
2021-07-06 14:14:25 +0200

    add context provider to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AppProvider } from "./components/AppContext";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Dashboard from "./components/Dashboard";
@@ -7,22 +8,24 @@ import About from "./components/About";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <main>
-          <Switch>
-            <Route path="/" exact>
-              <Dashboard />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-          </Switch>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <AppProvider>
+      <Router>
+        <div className="app">
+          <Header />
+          <main>
+            <Switch>
+              <Route path="/" exact>
+                <Dashboard />
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+            </Switch>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </AppProvider>
   );
 }
 
