66c4270b30a89580a44c012926651effecf7d770
Pascal Pater <pascal.pater@gmail.com>
2021-07-13 20:07:51 +0200

    add checkbox to studentItem; update checked property when checbox is clicked

--- src/components/StudentItem.js
+++ src/components/StudentItem.js
@@ -4,10 +4,31 @@ import { Link } from "react-router-dom";
 
 const StudentItem = () => {
   
-  const { studentNames } = useContext(AppContext);
+  const { students, setStudents, studentNames, setStudentNames } = useContext(AppContext);
+
+  const studentChecked = (event) => {
+    const value = event.target.value;
+    const studentsCopy = [...students];
+    const studentsCheckedList = studentsCopy.map((student) => {
+      if (student.name === value) {
+        student.checked = !student.checked
+      }
+      return student
+    })
+    setStudents(studentsCheckedList);
+  };
+
+  console.log("Student selected is:", students)
 
   const itemsList = studentNames.map((student) => (
     <li key={student.id}>
+      <input
+        type="checkbox"
+        id={student.id}
+        value={student.name}
+        checked={student.checked}
+        onChange={(event) => studentChecked(event)}
+      />
       <Link to={`/student/${student.name}`}>{student.name}</Link>
     </li>
   ));
