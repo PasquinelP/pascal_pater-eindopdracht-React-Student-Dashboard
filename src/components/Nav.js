6029ffe0e1298c797b685ba9671a67bafe892c1b
Pascal Pater <pascal.pater@gmail.com>
2021-07-19 20:12:02 +0200

    remove dubble nav item title

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ const Nav = () => {
     <nav className="app-nav">
       <ul className="app-nav__list">
         <NavLink to="/" exact activeClassName="selected">
-          <li className="app-nav__item">Home Dashboard</li>
+          <li className="app-nav__item">Dashboard</li>
         </NavLink>
         <NavLink to="/about" activeClassName="selected">
           <li className="app-nav__item">About</li>
