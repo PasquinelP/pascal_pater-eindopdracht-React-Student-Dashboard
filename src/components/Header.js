9a2f90c8b86116598a2dbef428c138cdef84533c
Pascal Pater <pascal.pater@gmail.com>
2021-07-20 16:49:39 +0200

    update header classes, add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import logo from "../assets/winc-logo.png";
 
 const Header = () => {
   return (
     <header>
-      <div className="page-header">
-        <h1>Winc Student Dashboard</h1>
+      <div className="app__header">
+        <img src={logo} className="app__logo" alt="Winc Student Dashboard logo" />
+        <h1 className="app__title">Winc Student Dashboard</h1>
       </div>
     </header>
   );
