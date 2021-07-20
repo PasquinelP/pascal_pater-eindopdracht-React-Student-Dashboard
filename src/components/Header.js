43322443b2dfe154830fbb34a1fe9503385c9fc0
Pascal Pater <pascal.pater@gmail.com>
2021-07-20 19:13:01 +0200

    add link on logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import logo from "../assets/winc-logo.png";
 
 const Header = () => {
   return (
     <header>
       <div className="app__header">
-        <img src={logo} className="app__logo" alt="Winc Student Dashboard logo" />
+        <NavLink to="/" exact>
+          <img src={logo} className="app__logo" alt="Winc Student Dashboard logo" />
+        </NavLink>
         <h1 className="app__title">Winc Student Dashboard</h1>
       </div>
     </header>
