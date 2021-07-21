0de7514c179dbacd9e0685a903f2684974183bd7
Pascal Pater <pascal.pater@gmail.com>
2021-07-21 20:07:21 +0200

    add classes, remove test link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,13 +4,10 @@ import StudentList from "./StudentList";
 
 const Nav = () => {
   return (
-    <nav className="app-nav">
+    <nav className="panel panel--action app-nav">
       <ul className="app-nav__list">
         <NavLink to="/" exact activeClassName="selected">
           <li className="app-nav__item">Dashboard</li>
-        </NavLink>
-        <NavLink to="/about" activeClassName="selected">
-          <li className="app-nav__item">About</li>
         </NavLink>
         <StudentList />
       </ul>
