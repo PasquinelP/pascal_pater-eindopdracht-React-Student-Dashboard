2c776f65942fb84cc0700beb5da8264360093d0c
Pascal Pater <pascal.pater@gmail.com>
2021-07-21 17:29:21 +0200

    add select assignments to student page

--- src/components/Student.js
+++ src/components/Student.js
@@ -3,17 +3,19 @@ import { useParams } from "react-router-dom";
 import Nav from "./Nav";
 import Chart from './Chart';
 import SortRating from "./SortRating";
+import SelectAssignment from "./SelectAssignment";
 
 const Student = () => {
   const { name } = useParams();
 
   return (
-    <div>
+    <>
       <Nav />
       <h1 className="page-title">Rating per opdracht van {name}</h1>
       <Chart />
       <SortRating />
-    </div>
+      <SelectAssignment />
+    </>
   );
 };
 
